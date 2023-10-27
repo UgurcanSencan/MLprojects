00fea166677bd657a05d8f7c9902468c131ad1f1
UgurcanSencan <89963170+UgurcanSencan@users.noreply.github.com>
2023-10-27 10:13:27 +0000

    update

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 from flask import Flask, render_template, request, jsonify
-from utils import make_prediction
-
+from utils import model_predict
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/predict", methods=["POST"])
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    email = request.form.get('email-content')
-    prediction = make_prediction(email)
+    email = request.form.get('content')
+    prediction = model_predict(email)
     return render_template("index.html", prediction=prediction, email=email)
 
-@app.route("/api/predict", methods=["POST"])
-def predict_api():
-    data = request.get_json(force=True)
-    email = data["content"]
-    prediction = make_prediction(email)
-    return jsonify({'prediction':prediction, 'email':email})
-
+# Create an API endpoint
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email = data['content']
+    prediction = model_predict(email)
+    return jsonify({'prediction': prediction, 'email': email})  # Return prediction
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
-
-
-
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ cv = pickle.load(open("models/cv.pkl","rb")) # tokenizer/preprocessing
 clf = pickle.load(open("models/clf.pkl","rb")) # model
 
 
-def make_prediction(email):
+def model_predict(email):
     tokenized_email = cv.transform([email]) # X 
     prediction = clf.predict(tokenized_email) # y_predicted
     prediction = 1 if prediction == 1 else -1
